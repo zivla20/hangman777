b87c92fe07f469058f11e9adfaa7ee04cd3f83a9
zivla20 <88274836+zivla20@users.noreply.github.com>
2024-06-04 20:20:50 +0300

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -1,8 +1,27 @@
-condition = True 
+import random
 
-while condition == True:
-  guess = input("Please enter a single character: ")
-  if len(guess) == 1 and guess.isalpha():
-    condition == False
+word_list = ["raspberry", "mango", "pineapple", "blackberry", "grape"]
+word = random.choice(word_list)
+
+def ask_for_input():
+#This function checks if the input is valid.
+  condition = True 
+  while condition == True:
+    guess = input("Please enter a single character: ")
+    if len(guess) == 1 and guess.isalpha():
+      condition = False
+    else:
+      print("Invalid letter. Please enter a single aphabetical character.")
+  
+  return guess
+
+def check_guess(guess):
+  guess = guess.lower()
+  if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+    check_guess(guess)
   else:
-    print("Invalid letter. Please enter a single aphabetical character.")
+    print("Sorry, {guess} is not in the word. Try again.")
+
+
+
