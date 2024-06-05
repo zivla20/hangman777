9af0b0ddad17ab7d8eb61985e97916ec5aec600d
zivla20 <88274836+zivla20@users.noreply.github.com>
2024-06-05 20:11:11 +0300

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -10,3 +10,36 @@ class Hangman:
     self.num_letters = len(set(self.word))
     self.list_of_guesses = []
     
+  def check_guess(self, guess):
+    guess = guess.lower()
+    if guess in self.word:
+      print(f"Good guess! {guess} is in the word.")
+      return True
+    else:
+      print("Sorry, {guess} is not in the word. Try again.")
+      return False
+
+  def ask_for_input(self):
+  #This function checks if the input is valid. 
+    while True:
+      guess = input("Please enter a single character: ")
+      if len(guess) != 1 or not guess.isalpha():
+        print("Invalid letter. Please enter a single aphabetical character.")
+      elif guess in self.list_of_guesses:
+        print("You already tried that letter!")
+      else:
+        self.check_guess(guess)
+        self.list_of_guesses.append(guess)
+        break
+
+word_list = ['apple', 'banana', 'cherry']
+game = Hangman(word_list)
+game.ask_for_input()
+
+
+
+
+
+
+
+
