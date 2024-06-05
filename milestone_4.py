1d984fdab13a9a2d92cd21255aeaf31511da9920
zivla20 <88274836+zivla20@users.noreply.github.com>
2024-06-05 20:19:31 +0300

    Define what happens if letter is not in word.

--- milestone_4.py
+++ milestone_4.py
@@ -20,7 +20,9 @@ class Hangman:
         self.num_letters -= 1
       return True
     else:
-      print("Sorry, {guess} is not in the word. Try again.")
+      print(f"Sorry, {guess} is not in the word.")
+      self.num_lives -= 1
+      print(f"You have {self.num_lives} lives left.")
       return False
 
   def ask_for_input(self):
